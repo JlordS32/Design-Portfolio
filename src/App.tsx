70026a3ef761bde2eeff95e74074fcf1e28bbcd9
JlordS32 <jlordsty128@gmail.com>
2025-01-28 09:23:42 +1100

    setup grid for hero

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import styles from "./styles/modules/Main.module.scss";
 
 const App = () => {
    return (
-      <main>
+      <main className={styles.main}>
          <Navbar />
          <Hero />
       </main>
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,12 +1,47 @@
+import styles from "../styles/modules/Hero.module.scss";
+
 const Hero = () => {
    return (
-      <section className="hero">
-         <div className="title">
+      <section className={styles.hero}>
+         <div className={styles.title}>
             <h1>Design solutions made easy</h1>
             <p>
                With over ten years of experience in various design disciplines,
                I’m your one-stop shop for your design needs.
             </p>
+         </div>
+
+         <div className={styles.graphics}>
+            <div className={styles.gridWrapper}>
+               <div data-content="Graphic Design">
+                  <span>Graphic Design</span>
+               </div>
+            </div>
+            <div className={styles.gridWrapper}>
+               <div data-content="UI/UX">
+                  <span>UI/UX</span>
+               </div>
+            </div>
+            <div className={styles.gridWrapper}>
+               <div data-content="Apps">
+                  <span>Apps</span>
+               </div>
+            </div>
+            <div className={styles.gridWrapper}>
+               <div data-content="Photography">
+                  <span>Photography</span>
+               </div>
+            </div>
+            <div className={styles.gridWrapper}>
+               <div data-content="Illustrations">
+                  <span>Illustrations</span>
+               </div>
+            </div>
+            <div className={styles.gridWrapper}>
+               <div data-content="Motion graphics">
+                  <span>Motion graphics</span>
+               </div>
+            </div>
          </div>
       </section>
    );
