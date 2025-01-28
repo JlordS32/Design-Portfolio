45ed538681988f0e52b1dfc88ef7e87d6aad7636
JlordS32 <jlordsty128@gmail.com>
2025-01-29 07:55:51 +1100

    done with the desktop layout

---
+++ src/components/Contact.tsx
@@ -0,0 +1,23 @@
+import styles from "../styles/modules/Contact.module.scss";
+import Button from "./Button";
+
+const Contact = () => {
+   return (
+      <section className={styles.contact}>
+         <div className={styles.booking}>
+            <h2>Book a call with me</h2>
+            <p>
+               I’d love to have a chat to see how I can help you. The best first
+               step is for us to discuss your project during a free
+               consultation. Then we can move forward from there.
+            </p>
+         </div>
+
+         <div className={styles.clickToAction}>
+            <Button text="Free Consultation" buttonType="secondary" />
+         </div>
+      </section>
+   );
+};
+
+export default Contact;
